2cf39be87496f6db496d6fb276aa30a26d4787a3
stealth35 <jinmoku@hotmail.com>
2011-02-25 15:20:38 +0100

    add files

---
+++ Stdlib/SplFileObject.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @namespace
+ */
+namespace Stdlib;
+
+/**
+ * Add fputcsv method to \SplFileObject
+ * 
+ * @category Stdlib
+ * @package  Stdlib
+ * @author   stealth35
+ * @see      http://bugs.php.net/bug.php?id=53264
+ */
+class SplFileObject extends \SplFileObject
+{
+    /**
+     * Format line as CSV and write to file
+     * 
+     * @param array $fields
+     * @param string $delimiter optional
+     * @param string $enclosure optional
+     * 
+     * @return mixed
+     */
+    public function fputcsv(array $fields, $delimiter = ',', $enclosure = '"')
+    {
+        $handle = fopen('php://memory', 'r+');
+
+        if(false === fputcsv($handle, $fields, $delimiter, $enclosure))
+        {
+            return false;
+        }
+
+        rewind($handle);
+
+        return $this->fwrite(stream_get_contents($handle));
+    }
+}
