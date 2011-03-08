8832eee383a352d54027cf6c6b0f01164f19e503
stealth35 <jinmoku@hotmail.com>
2011-03-08 08:17:31 -0800

    add preserve keys option

--- library/Stdlib/ReverseIterator.php
+++ library/Stdlib/ReverseIterator.php
@@ -4,16 +4,18 @@ namespace Stdlib;
 class ReverseIterator implements \IteratorAggregate
 {
     private $_iterator;
+    private $_preserve_keys;
 
-    public function __construct(\Traversable $iterator)
+    public function __construct(\Traversable $iterator, $preserve_keys = false)
     {
         $this->_iterator = $iterator;
+        $this->_preserve_keys = $preserve_keys;
     }
 
     public function getIterator()
     {
-        $array = iterator_to_array($this->_iterator, true);
-        $reverse = array_reverse($array, true);
+        $array = iterator_to_array($this->_iterator, $this->_preserve_keys);
+        $reverse = array_reverse($array, $this->_preserve_keys);
 
         return new \ArrayIterator($reverse);
     }
