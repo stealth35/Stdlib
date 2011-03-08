e89a4ed537e1410b4ab1215f5b0ca611682e3d65
stealth35 <jinmoku@hotmail.com>
2011-03-08 17:00:17 +0100

    add ReverseIterator

---
+++ library/Stdlib/ReverseIterator.php
@@ -0,0 +1,18 @@
+<?php
+class ReverseIterator implements IteratorAggregate
+{
+    private $_iterator;
+
+    public function __construct(Traversable $iterator)
+    {
+        $this->_iterator = $iterator;
+    }
+
+    public function getIterator()
+    {
+        $array = iterator_to_array($this->_iterator, true);
+        $reverse = array_reverse($array, true);
+
+        return new ArrayIterator($reverse);
+    }
+}
