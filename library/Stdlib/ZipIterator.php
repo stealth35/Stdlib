a8a34f2d3e14483a2d3b34847a50c2d0cea079e1
unknown <croux@.ecole.root.priv>
2011-03-08 16:25:09 +0100

    add Zipiterator

---
+++ library/Stdlib/ZipIterator.php
@@ -0,0 +1,62 @@
+<?php
+class ZipIterator implements Iterator
+{
+    private $_resource;
+    private $_filename;
+    private $_current;
+    private $_position;
+
+    public function __construct($filename)
+    {
+        $this->_filename = $filename;
+        $this->_resource = zip_open($this->_filename);
+
+        if(false === is_resource($this->_resource))
+        {
+            throw new RuntimeException('zip error', $this->_resource);
+        }
+
+        $this->_current = zip_read($this->_resource);
+        $this->position = 0;
+    }
+
+    public function current()
+    {
+        return $this->_current;
+    }
+
+    public function key()
+    {
+        return $this->_position;
+    }
+
+    public function next()
+    {
+        $this->_current = zip_read($this->_resource);
+        $this->_position++;
+
+        return $this->_current;
+    }
+
+    public function rewind()
+    {
+        if($this->_resource)
+        {
+            zip_close($this->_resource);
+        }
+
+        $this->_resource = zip_open($this->_filename);
+        $this->_current  = zip_read($this->_resource);
+        $this->_position = 0;
+    }
+
+    public function valid()
+    {
+        return is_resource($this->_current);
+    }
+
+    public function __destruct()
+    {
+        zip_close($this->_resource);
+    }
+}
