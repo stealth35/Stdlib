8e0f68a12cb609321929dc4688aaefec79ab16ed
stealth35 <jinmoku@hotmail.com>
2011-04-10 18:27:00 -0700

    Simplify PDOIterator

--- library/Stdlib/PDOIterator.php
+++ library/Stdlib/PDOIterator.php
@@ -9,60 +9,20 @@ namespace Stdlib;
  * @package  Stdlib
  * @author   stealth35
  */
-class PDOIterator implements \Iterator
+class PDOIterator extends \NoRewindIterator implements \IteratorAggregate
 {
     private $_stmt;
-    private $_current;
-    private $_position;
-    private $_fetch_style;
-    private $_cursor_orientation;
-    private $_cursor_offset;
-    
+
     /**
      * @param \PDOStatement $stmt
-     * @param int $fetch_style optional
-     * @param int $cursor_orientation optional
-     * @param int $cursor_offset optional
      */
-    public function __construct(\PDOStatement $stmt, $fetch_style = null, $cursor_orientation = null, $cursor_offset = null)
+    public function __construct(\PDOStatement $stmt)
     {
-        $this->_stmt               = $stmt;
-        $this->_fetch_style        = $fetch_style;
-        $this->_cursor_orientation = $cursor_orientation;
-        $this->_cursor_offset      = $cursor_offset;
-        
-        $this->_position = -1;
-        $this->next();
-    }
-    
-    public function current()
-    {
-        return $this->_current;
+        $this->_stmt = $stmt;
     }
 
-    public function key()
+    public function getIterator()
     {
-        return $this->_position;
-    }
-
-    public function next()
-    {
-        $this->_current = $this->_stmt->fetch($this->_fetch_style, $this->_cursor_orientation, $this->_cursor_offset);
-        ++$this->_position;
-    }
-
-    public function rewind()
-    {
-        return;
-    }
-
-    public function valid()
-    {
-        return $this->_current;
-    }
-
-    public function __destruct()
-    {
-        $this->_stmt->closeCursor();
+        return new IteratorIterator($this->_stmt);
     }
 }
