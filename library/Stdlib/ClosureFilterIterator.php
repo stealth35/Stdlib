de501d9c2f0c7735f049add9a79bc6238d9a1dc5
stealth35 <jinmoku@hotmail.com>
2011-05-03 11:16:02 +0200

    Added ClosureFilterIterator

---
+++ library/Stdlib/ClosureFilterIterator.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @namespace
+ */
+namespace Stdlib;
+
+/**
+ * @category Stdlib
+ * @package  Stdlib
+ * @author   stealth35
+ */
+class ClosureFilterIterator extends FilterIterator
+{
+    /**
+     * @var \Closure
+     */
+    private $_closure;
+
+    /**
+     * @param \Iterator $iterator
+     * @param \Closure $closure
+     */
+    public function __construct(\Iterator $iterator, \Closure $closure)
+    {
+        parent::__construct($iterator);
+        $this->_closure = $closure;    
+    }
+
+    public function accept()
+    {
+        $closure = $this->_closure;
+        return $closure($this->getInnerIterator()->current());
+    }
+}
