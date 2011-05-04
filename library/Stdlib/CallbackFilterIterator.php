11bb4cda6d664e03feeb4bfda6a005e9f87e8fa1
stealth35 <jinmoku@hotmail.com>
2011-05-04 16:33:59 +0200

    Added CallbackFilterIerator

---
+++ library/Stdlib/CallbackFilterIterator.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @namespace
+ */
+namespace Stdlib;
+
+/**
+ * @category Stdlib
+ * @package  Stdlib
+ * @author   stealth35
+ */
+class CallbackFilterIerator extends \FilterIterator
+{
+    /**
+     * @var mixed
+     */
+    private $_callback;
+
+    /**
+     * @param \Iterator $iterator
+     * @param mixed $callback
+     */
+    public function __construct(\Iterator $iterator, $callback)
+    {
+        parent::__construct($iterator);
+        $this->_callback = $callback;    
+    }
+
+    public function accept()
+    {
+        return call_user_func($this->_callback, $this->getInnerIterator()->current());
+    }
+}
