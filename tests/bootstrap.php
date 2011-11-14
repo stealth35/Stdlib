34a6be78094e6f732b3983e2f356d067c78378e3
stealth35 <jinmoku@hotmail.com>
2011-11-14 18:54:06 +0100

    Typo

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ $src = __DIR__.'/../src/';
 
 spl_autoload_register(function($class) use ($src) {
     $filename = $src.str_replace('\\', '/', $class).'.php';
-    if (file_exists($filename) {
+    if (file_exists($filename)) {
         require $filename;
     }
 });
